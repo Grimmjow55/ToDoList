8402ad98257818cd4bef2958c5ba7e4e758a6a05
Grimmjow <nathan_randoulet@msn.com>
2024-06-20 10:41:49 +0200

    ajout du projet

---
+++ Data/TpTodoListDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TpTodoList.Models;
+
+namespace TpTodoList.Data
+{
+
+    public class TpTodoListDbContext : DbContext
+    {
+        public DbSet<TpTodoList.Models.Task> Tasks { get; set; }
+        public TpTodoListDbContext(DbContextOptions<TpTodoListDbContext> options)
+            : base(options)
+        {
+        }
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TpTodoList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
